d8bd669a316406c6416c5c2fade0bca95d7fe10e
tdnguyengithub1986 <tdnguyenlearntoprogram@gmail.com>
2017-09-06 16:26:16 +0700

    First Commit

---
+++ src/main/java/com/tdn/DAO/DBInfo.java
@@ -0,0 +1,50 @@
+package com.tdn.DAO;
+
+
+
+
+public class DBInfo {
+	
+	static String dbHost = "mysql";
+	static String dbName = "tdn";
+	static int dbPort = 3306;
+	
+	static String dbURL = "jdbc:mysql:" + dbHost + ":" + dbPort +"/" +dbName;
+	
+	static String rootUserName = "root";
+	
+	static String rootPassword = "tdnrootpassword";
+	
+	static String jdbcDriver = "com.msql.jdbc.Driver";
+
+	public static String getDbHost() {
+		return dbHost;
+	}
+
+	public static String getDbName() {
+		return dbName;
+	}
+
+	public static int getDbPort() {
+		return dbPort;
+	}
+
+	public static String getDbURL() {
+		return dbURL;
+	}
+
+	public static String getRootUserName() {
+		return rootUserName;
+	}
+
+	public static String getRootPassword() {
+		return rootPassword;
+	}
+
+	public static String getJdbcDriver() {
+		return jdbcDriver;
+	}
+	
+	
+	
+}
--- src/main/java/com/tdn/Services/LoginService.java
+++ src/main/java/com/tdn/Services/LoginService.java
@@ -1,10 +1,34 @@
 package com.tdn.Services;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
 import org.springframework.stereotype.Service;
+
+import com.tdn.DAO.DBInfo;
 
 @Service
 public class LoginService {
 	public boolean validate(String userName, String password){
-		return userName.equals("administrator")&&password.equals("default");
+		
+		try{
+			Class.forName(DBInfo.getJdbcDriver());
+			Connection dbConnection = DriverManager.getConnection(DBInfo.getDbURL(), DBInfo.getRootUserName(), DBInfo.getRootPassword());
+			PreparedStatement ps = dbConnection.prepareStatement("SELECT * FROM USER WHERE USERNAME=? AND PASSWORD=?");
+			ps.setString(1, userName);
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()){
+				return true;
+			}
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		
+		return false;
 	}
 }
